7cffecfdd22ff1dda2a9fd40c6c2fc9ade4c2723
Michael Rosa Jr <mrosa2000@yahoo.com>
2022-07-27 15:35:51 -0500

    Complete qualified assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,52 @@
+const { getBooksBorrowedCount } = require("./home");
+
+function findAuthorById(authors, id) {
+  //return author with corresponding ID
+  return _findElementById(authors, id);
+}
+
+function findBookById(books, id) {
+  //use the findElementById helper function I wrote
+  return _findElementById(books, id);
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  const returned = true;
+  const borrowed = !returned;
+  //use _filterBorrowed helper function to create filtered arrays of all books that are either borrowed or returned
+  const borrowedBooks = _filterBorrowed(books, borrowed);
+  const returnedBooks = _filterBorrowed(books, returned);
+  //return an array that spreads both of the arrays
+  return [[...borrowedBooks], [...returnedBooks]];
+}
+
+function getBorrowersForBook({ borrows }, accounts) {
+  //array we will populate and return
+  const borrowers = [];
+  // iterate through each record in borrows
+  for (let record in borrows) {
+    //find matching account using helper function
+    const borrowId = borrows[record].id;
+    const matchingAccount = _findElementById(accounts, borrowId);
+    borrowers.push({ ...borrows[record], ...matchingAccount });
+  }
+  //truncate to just the first ten elements and return the array
+  return borrowers.slice(0, 10);
+}
+
+//Helper function I wrote to easily find an element in a provided array given an "id" value
+function _findElementById(elements, id) {
+  return elements.find((element) => element.id === id);
+}
+
+//Helper Function to make partitionBooksByBorrowed Status more readable and filter out a list of books based on their "returned" status
+function _filterBorrowed(books, status) {
+  return books.filter(({ borrows }) => status === borrows[0].returned);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
---
+++ public/src/home.js
@@ -0,0 +1,85 @@
+function getTotalBooksCount(books) {
+  //use helperfunction to easily count books
+  return arrayItemCount(books);
+}
+
+function getTotalAccountsCount(accounts) {
+  //use helperfunction to easily count accounts
+  return arrayItemCount(accounts);
+}
+
+function getBooksBorrowedCount(books) {
+  //use reduce to iterate through each book in books
+  return books.reduce((borrowCount, { borrows }) => {
+    //If the most recent transaction in borrows has not been returned, then increase the total borrow counter.
+    const mostRecent = borrows[0];
+    if (!mostRecent.returned) borrowCount++;
+    return borrowCount;
+  }, 0);
+}
+
+function getMostCommonGenres(books) {
+  //Use __sortNSlice helper function to sort by highest and truncate to just top 5
+  return _sortNSlice(
+    books
+      //iterate through each book in books
+      .reduce((genres, book) => {
+        //check the array we are currently constructing to see if we already have this genre
+        const matchingGenre = genres.find((genre) => genre.name === book.genre);
+        //if we don't have a match, push it into our genres array with count 1, otherwise increase the current count by 1
+        !matchingGenre
+          ? genres.push({ name: book.genre, count: 1 })
+          : matchingGenre.count++;
+        return genres;
+      }, [])
+  );
+}
+
+function getMostPopularBooks(books) {
+  //Use _sortNSlice helper function to sort by highest and truncate to just top 5
+  return _sortNSlice(
+    //map through each book in books
+    books.map(({ title, borrows }) => ({
+      name: title, //name is the title of the book we deconstructed
+      count: arrayItemCount(borrows),
+    }))
+  );
+}
+
+function getMostPopularAuthors(books, authors) {
+  //Use _sortNSlice helper function to sort by highest and truncate to just top 5
+  return _sortNSlice(
+    //map through each author in authors
+    authors.map(({ name: { first, last }, id }) => ({
+      name: `${first} ${last}`,
+      count: _authorBorrows(books, id), 
+    }))
+  );
+}
+
+function _sortNSlice(arr, slicer = 5) {
+  const newArr = [...arr];
+  return newArr
+    .sort(({ count: count1 }, { count: count2 }) => count2 - count1)
+    .slice(0, slicer);
+}
+
+function _authorBorrows(books, id) {
+  return books.reduce((totalBorrows, { authorId, borrows }) => {
+    if (authorId === id) totalBorrows += arrayItemCount(borrows);
+    return totalBorrows;
+  }, 0);
+}
+
+function arrayItemCount(item) {
+  return item.length;
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
